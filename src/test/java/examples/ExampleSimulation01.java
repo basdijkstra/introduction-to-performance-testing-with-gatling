0c05fe4f35a29c43cf718405a35c0f5f7d8a264a
Bas Dijkstra <bas@ontestautomation.com>
2025-04-10 10:39:20 +0200

    Complete first example

--- src/test/java/examples/ExampleSimulation01.java
+++ src/test/java/examples/ExampleSimulation01.java
@@ -1,4 +1,4 @@
-package example;
+package examples;
 
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.*;
@@ -8,30 +8,38 @@ import io.gatling.javaapi.http.*;
 
 import java.time.Duration;
 
-public class BasicSimulation extends Simulation {
+public class ExampleSimulation01 extends Simulation {
 
   // Load VU count from system properties
   // Reference: https://docs.gatling.io/guides/passing-parameters/
-  private static final int vu = Integer.getInteger("vu", 1000);
+  private static final int usersPerSecond = 2;
+  private static final int durationInSeconds = 30;
 
   // Define HTTP configuration
   // Reference: https://docs.gatling.io/reference/script/protocols/http/protocol/
-  private static final HttpProtocolBuilder httpProtocol = http.baseUrl("https://api-ecomm.gatling.io")
+  private static final HttpProtocolBuilder httpProtocol = http.baseUrl("https://api.zippopotam.us")
       .acceptHeader("application/json")
       .userAgentHeader(
           "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36");
 
   // Define scenario
   // Reference: https://docs.gatling.io/reference/script/core/scenario/
-  private static final ScenarioBuilder scenario = scenario("Scenario").exec(http("Session").get("/session"));
+  private static final ScenarioBuilder scenario =
+          scenario("GET zip code data")
+                  .exec(http("Data for US zip code 90210")
+                          .get("/us/90210"));
 
   // Define assertions
   // Reference: https://docs.gatling.io/reference/script/core/assertions/
-  private static final Assertion assertion = global().failedRequests().count().lt(1L);
+  private static final Assertion assertion =
+          global().failedRequests().count().lt(1L);
 
   // Define injection profile and execute the test
   // Reference: https://docs.gatling.io/reference/script/core/injection/
   {
-    setUp(scenario.injectOpen(rampUsers(vu).during(Duration.ofMinutes(2)))).maxDuration(Duration.ofMinutes(3)).assertions(assertion).protocols(httpProtocol);
+    setUp(scenario.injectOpen(constantUsersPerSec(usersPerSecond)
+            .during(Duration.ofSeconds(durationInSeconds))))
+            .assertions(assertion)
+            .protocols(httpProtocol);
   }
 }
